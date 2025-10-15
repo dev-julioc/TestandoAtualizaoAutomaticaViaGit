917b1352df9e695c9c943e97d9d88fe44b4576b9
Julio Cesar de Jesus <julio.c.jesus@hotmail.com>
2025-10-15 19:00:05 -0300

    3.0.0

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -41,7 +41,7 @@
             label1.Name = "label1";
             label1.Size = new Size(316, 72);
             label1.TabIndex = 0;
-            label1.Text = "Versão 2.2.5";
+            label1.Text = "Versão 3.0.0";
             // 
             // button1
             // 
